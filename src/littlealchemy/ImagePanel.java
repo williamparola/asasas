8b31d15cb1823a1af9f25c52534ea8f138bb999b
william.parola <william.parola@InfoTele24.maxwell.local>
2023-04-03 11:41:44 +0200

    Quasi finito

--- src/littlealchemy/ImagePanel.java
+++ src/littlealchemy/ImagePanel.java
@@ -3,6 +3,7 @@ package littlealchemy;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -10,28 +11,49 @@ import javax.swing.JPanel;
 
 public class ImagePanel extends JPanel{
     
-    ImageIcon image = new ImageIcon();
+    ImageIcon image = new ImageIcon("/src/skull.JPG");
     //"minuto 3:40"
+    // anche 5
     
     final int IMG_WIDTH = image.getIconWidth();
     final int IMG_HEIGHT = image.getIconHeight();
     
     Point image_corner;
-    Point previusPoint;
+    Point previousPoint;
     
     ImagePanel(){
-        image_corner =
+        image_corner = new Point(0,0);
+        
+        ClickListener clickListener = new ClickListener();
+        this.addMouseListener(clickListener);
+        
+        DragListener dragListener = new DragListener();
+        this.addMouseMotionListener(dragListener);
     }
     
     public void paintComponent(Graphics g){
         
+        super.paintComponent(g);
+        image.paintIcon(this, g, (int)image_corner.getX(), (int)image_corner.getY());
     }
     
     private class ClickListener extends MouseAdapter{
         
+        public void mousePressed(MouseEvent evt){
+            previousPoint = evt.getPoint();
+        }
     }
     
     private class DragListener extends MouseMotionAdapter{
         
+        public void mouseDragged(MouseEvent evt) {
+            Point currentPoint = evt.getPoint();
+            image_corner.translate(
+                    (int)(currentPoint.getX() - previousPoint.getX()),
+                    (int)(currentPoint.getY() - previousPoint.getY())
+            );
+            previousPoint=currentPoint;
+            repaint();
+        }
     }
 }
