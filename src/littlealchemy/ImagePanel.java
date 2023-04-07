eae685207ffef94a02c4014f84fcdbdbcb06a613
jcmaxwell <jcmaxwell@DESKTOP-2UBS3TN.homenet.telecomitalia.it>
2023-04-07 17:25:26 +0200

    pain

--- src/littlealchemy/ImagePanel.java
+++ src/littlealchemy/ImagePanel.java
@@ -12,17 +12,23 @@ import javax.swing.JPanel;
 public class ImagePanel extends JPanel{
     
     ImageIcon image = new ImageIcon("fuocoMoltoBello.png");
+    ImageIcon imageT = new ImageIcon("skull.JPG");
     //"minuto 3:40"
     // anche 5
     
     final int IMG_WIDTH = image.getIconWidth();
     final int IMG_HEIGHT = image.getIconHeight();
+    final int IMG_WIDTH_T = imageT.getIconWidth();
+    final int IMG_HEIGHT_T = imageT.getIconHeight();
     
     Point image_corner;
+    Point image_cornerT;
     Point previousPoint;
+    Point previousPointT;
     
     ImagePanel(){
         image_corner = new Point(0,0);
+        image_cornerT = new Point(500,0);
         
         ClickListener clickListener = new ClickListener();
         this.addMouseListener(clickListener);
@@ -32,15 +38,20 @@ public class ImagePanel extends JPanel{
     }
     
     public void paintComponent(Graphics g){
+        //Graphics g1 = null;
         
         super.paintComponent(g);
+        //super.paintComponent(g1);
         image.paintIcon(this, g, (int)image_corner.getX(), (int)image_corner.getY());
+        
+        imageT.paintIcon(this, g, (int)image_cornerT.getX(), (int)image_cornerT.getY());
     }
     
     private class ClickListener extends MouseAdapter{
         
         public void mousePressed(MouseEvent evt){
             previousPoint = evt.getPoint();
+            previousPointT = evt.getPoint();
         }
     }
     
@@ -48,11 +59,17 @@ public class ImagePanel extends JPanel{
         
         public void mouseDragged(MouseEvent evt) {
             Point currentPoint = evt.getPoint();
+            Point currentPointT = evt.getPoint();
             image_corner.translate(
                     (int)(currentPoint.getX() - previousPoint.getX()),
                     (int)(currentPoint.getY() - previousPoint.getY())
             );
+            image_cornerT.translate(
+                    (int)(currentPointT.getX() - previousPointT.getX()),
+                    (int)(currentPointT.getY() - previousPointT.getY())
+            );
             previousPoint=currentPoint;
+            previousPointT=currentPointT;
             repaint();
         }
     }
